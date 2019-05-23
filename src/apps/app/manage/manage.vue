0c7533567f8615ac58253e83a92cf2fc5ca6bc7c
xiaoyankuaipao <511657675@qq.com>
2019-05-23 19:27:55 +0800

    change somethis

--- src/apps/app/manage/manage.vue
+++ src/apps/app/manage/manage.vue
@@ -26,11 +26,43 @@
 
         <div class="content">
             <!-- 导航 -->
-            <div>
+            <div :class="{'el-col':true,'el-col-4':true,'navigation':true,'collapsed':menuCollapsed}">
                 <!-- 菜单 -->
-                <el-menu>
-
-                </el-menu>
+                 <el-menu
+                    default-active="2"
+                    class="el-menu-vertical-demo"
+                    @open="handleOpen"
+                    @close="handleClose"
+                    background-color="#324157"
+                    text-color="#fff"
+                    active-text-color="#ffd04b">
+                    <el-submenu index="1">
+                        <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>导航一</span>
+                        </template>
+                        <el-menu-item-group>
+                        <template slot="title">分组一</template>
+                        <el-menu-item index="1-1">选项1</el-menu-item>
+                        <el-menu-item index="1-2">选项2</el-menu-item>
+                        </el-menu-item-group>
+                        <el-menu-item-group title="分组2">
+                        <el-menu-item index="1-3">选项3</el-menu-item>
+                        </el-menu-item-group>
+                        <el-submenu index="1-4">
+                        <template slot="title">选项4</template>
+                        <el-menu-item index="1-4-1">选项1</el-menu-item>
+                        </el-submenu>
+                    </el-submenu>
+                    <el-menu-item index="2">
+                        <i class="el-icon-menu"></i>
+                        <span slot="title">导航二</span>
+                    </el-menu-item>
+                    <el-menu-item index="3">
+                        <i class="el-icon-setting"></i>
+                        <span slot="title">导航三</span>
+                    </el-menu-item>
+                    </el-menu>
                 <!-- 收缩按钮 -->
                 <div>
 
@@ -50,7 +82,8 @@ export default {
         return{
             mainMenus:['菜单一','菜单二','菜单三','菜单四'],
             hasPermission:false,
-            startLoading:false
+            startLoading:false,
+            menuCollapsed:false
         }
     },
     computed:{
